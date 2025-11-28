3f3ac744e9d8b75675a5633394ff974ec1898921
AIwithBert <willyebert@gmx.de>
2025-11-28 10:58:42 +0100

    Modify rotation duration and add left/right turns

--- Auto-projekt/Fahren.cpp
+++ Auto-projekt/Fahren.cpp
@@ -1,6 +1,8 @@
 //Auto Fahr Mechanik
-int drehdauer = 5;
-
+int drehdauer = 1000000;
+int drehunglinks = 5;
+int drehungrechts = 5;
+int anhalten;
 
 void setup() {
 
@@ -12,15 +14,23 @@ pinMode(5,OUTPUT);
 void loop() {
 for(int i = 0; i < drehdauer; i++)
 {
-analogWrite(3, 255);
+analogWrite(3, 210);
 analogWrite(5, 255);
-delay(1500);
-analogWrite(3,127);
-analogWrite(5,127);
-delay(1500);
-analogWrite(5, 0);
-analogWrite(5, 0);
-delay(1500);
+delay(500);
+
   
 }
+for(int i = 0; i < drehunglinks; i++)
+{
+  analogWrite(3,255);
+  analogWrite(5,127);
+  delay(500);
+}
+for(int i = 0; i < drehungrechts; i++)
+{
+  analogWrite(3,127);
+  analogWrite(5,255);
+  delay(500);
+}
 
+}
