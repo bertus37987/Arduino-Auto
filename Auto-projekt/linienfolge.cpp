9c1211966b0f22ac573055d7f6c2095bde26ed0b
AIwithBert <willyebert@gmx.de>
2025-12-08 12:50:00 +0100

    Add linienfolge.cpp for sensor input handling

---
+++ Auto-projekt/linienfolge.cpp
@@ -0,0 +1,15 @@
+int T1 = A1; //Rechts
+int T2 = A3; //Links
+
+void setup() {
+pinMode(7,OUTPUT);
+digitalWrite(7,HIGH);
+}
+
+void loop() {
+int rechts = digitalRead(T1);
+int links = digitalRead(T2);
+
+
+
+}
