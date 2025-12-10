c3b11a9296a0b37e4a3cdcbb32c377265b106c0c
AIwithBert <willyebert@gmx.de>
2025-12-10 08:31:13 +0100

    Aktualisieren von linienfolge.cpp

--- Auto-projekt/linienfolge.cpp
+++ Auto-projekt/linienfolge.cpp
@@ -10,6 +10,19 @@ void loop() {
 int rechts = digitalRead(T1);
 int links = digitalRead(T2);
 
+if (rechts >= 600){
+//Logik rechts fahren
+}
+else if (links >= 600){
+//Logik links fahren
+}
+else if (links <= 600 && rechts <= 600){
+//Logik gerade aus fahren
+}
+else {
+serial.println("Fehler mit der Linienfolge");
+}
+
 
 
 }
